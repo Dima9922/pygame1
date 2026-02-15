58bb2583f726fb9dadbc9bee69c529d988de382a
Dima9922 <nds15052007@gmail.com>
2026-02-15 11:28:17 +0200

    хмарки

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import pygame
 from scripts.entities import PhysicsEntity
 from scripts.utils import load_image, load_images
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
 class Game:
     def __init__(self):
@@ -25,16 +26,29 @@ class Game:
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-            'player': load_image("entities/player.png")}
+            'player': load_image("entities/player.png"),
+            'background': load_image("background.png"),
+            'clouds': load_images('clouds')}
+        
+        self.clouds = Clouds(self.assets['clouds'])
         
         self.tilemap = Tilemap(self, tile_size=16)
         
+        self.scroll = [0, 0]
+        
     def run(self):
         while True:
-            self.tilemap.render(self.display)
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 10
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 10
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+            
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll)
+            
+            self.tilemap.render(self.display, offset = render_scroll)
             
             self.player.update(self.tilemap, (self.movement[0] - self.movement[1], 0))
-            self.player.render(self.display)
+            self.player.render(self.display, offset = render_scroll)
  
             # event
             for event in pygame.event.get():
@@ -71,7 +85,7 @@ class Game:
                         self.bg_sound.stop() or self.bg_sound.play()"""
                       
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
-            self.display.fill("lightblue")
+            self.display.blit(self.assets['background'], (0, 0))
             pygame.display.update()
             self.clock.tick(60)
 Game().run()
